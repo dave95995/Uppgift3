c6830999778560262918e8fb52213369cba2a576
dave95995 <lex_dave@outlook.com>
2025-11-13 13:57:56 +0100

    Fix formatting

--- Uppgift3/Program.cs
+++ Uppgift3/Program.cs
@@ -5,37 +5,37 @@
 	När du skapar en Pokémon och försöker komma åt dess fält direkt – fungerar det? Varför
 	eller varför inte?
 
-	Använder publika properties och protected setters
+		Använder publika properties och protected setters
 
 	Om du senare vill lägga till en ny egenskap som gäller för alla Grass-typ Pokémon, var bör
 	du lägga den för att undvika repetition?
 
-	I GrassPokemon-klassen
+		I GrassPokemon-klassen
 
 	Om den nya egenskapen istället ska gälla för alla Pokémon, var är rätt plats att definiera
 	den?
 
-	I Pokemon-klassen
+		I Pokemon-klassen
 
 	Vad händer om du försöker lägga till en Charmander i en lista som bara tillåter
 	WaterPokemon?
 
-	Ger fel, endast pokemeons från WaterPokemon eller dess sub-klasser (Squirtle) är tillåtna.
+		Ger fel, endast pokemeons från WaterPokemon eller dess sub-klasser (Squirtle) är tillåtna.
 
 	Du vill lagra olika typer av Pokémon – Charmander, Squirtle och Bulbasaur – i samma lista.
-Vilken typ ska listan ha för att det ska fungera?
+	Vilken typ ska listan ha för att det ska fungera?
 
 		Pokemon
 
 	När du loopar genom listan och anropar Attack(), vad är det som gör att rätt
-attackbeteende körs för varje Pokémon?
+	attackbeteende körs för varje Pokémon?
 
-	Här valde jag att lägga till en kontroll vill tilldelning av listan i Pokemonklassen
-	där endast attacker av samma typ som Pokemon-objektet tilldelas
-	_attacks = value.Where(a => a.Type == Type).ToList();
+		Här valde jag att lägga till en kontroll vill tilldelning av listan i Pokemonklassen
+		där endast attacker av samma typ som Pokemon-objektet tilldelas
+		_attacks = value.Where(a => a.Type == Type).ToList();
 
 	Om du skapar en metod som bara finns på Bulbasaur, varför kan du inte anropa den direkt
-när den ligger i en List<Pokemon>? Hur skulle du ändå kunna komma åt den?
+	när den ligger i en List<Pokemon>? Hur skulle du ändå kunna komma åt den?
 
 			foreach (Pokemon pokemon in pokemons)
 			{
